687d5f9a65ef0423f1f2db6401c51eaf732d8232
bytyqinderim87@gmail.com <thegamechanger>
2022-02-01 11:24:38 +0100

    main: removed commandline runner

--- src/main/java/com/interjoin/teach/InterjoinTeachApplication.java
+++ src/main/java/com/interjoin/teach/InterjoinTeachApplication.java
@@ -1,18 +1,13 @@
 package com.interjoin.teach;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InterjoinTeachApplication implements CommandLineRunner {
+public class InterjoinTeachApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InterjoinTeachApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
