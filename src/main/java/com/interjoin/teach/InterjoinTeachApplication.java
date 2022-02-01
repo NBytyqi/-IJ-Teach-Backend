eb57b6951a923a38e592d27c8a14712b6d6eb15b
bytyqinderim87@gmail.com <thegamechanger>
2022-02-01 09:47:48 +0100

    main: project initilize

---
+++ src/main/java/com/interjoin/teach/InterjoinTeachApplication.java
@@ -0,0 +1,13 @@
+package com.interjoin.teach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterjoinTeachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterjoinTeachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/interjoin/teach/InterjoinTeachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interjoin.teach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterjoinTeachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
