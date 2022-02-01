21246412bde79c2817be729d27297747b8901245
bytyqinderim87@gmail.com <thegamechanger>
2022-02-01 09:55:03 +0100

    main: added command line runner

--- src/main/java/com/interjoin/teach/InterjoinTeachApplication.java
+++ src/main/java/com/interjoin/teach/InterjoinTeachApplication.java
@@ -1,13 +1,18 @@
 package com.interjoin.teach;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InterjoinTeachApplication {
+public class InterjoinTeachApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(InterjoinTeachApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
