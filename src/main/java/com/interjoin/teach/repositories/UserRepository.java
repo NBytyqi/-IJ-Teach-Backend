1705f43d7b004887a1b9e4302330adfacde5c052
Nderim.Bytyqi <bytyqinderim87@gmail.com>
2022-08-15 19:11:11 +0200

    message

--- src/main/java/com/interjoin/teach/repositories/UserRepository.java
+++ src/main/java/com/interjoin/teach/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.interjoin.teach.repositories;
 import com.interjoin.teach.dtos.interfaces.UserInterface;
 import com.interjoin.teach.entities.User;
 import com.interjoin.teach.enums.JoinAgencyStatus;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -33,7 +32,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     nativeQuery = true)
     List<UserInterface> getTeachersPerSubject(@Param("subjectId") Long subjectId);
 
-    @Query(value = "SELECT u.id, u.first_name as firstName, u.last_name as lastName, u.short_bio as shortBio, u.listed_price as listedPrice, u.rating, u.agency_name as agencyName, u.qualifications, u.long_bio as longBio, u.location as location, u.verified_teacher as verifiedTeacher from users u " +
+    @Query(value = "SELECT u.id, u.first_name as firstName, u.last_name as lastName, u.short_bio as shortBio, u.listed_price as listedPrice, u.rating, u.agency_name as agencyName, u.qualifications, u.long_bio as longBio, u.location as location, u.verified_teacher as verifiedTeacher, u.profile_picture as profilePicture from users u " +
             " inner join user_curriculum_subject ucs ON u.id = ucs.user_id WHERE ucs.subject_id = :subjectId AND ucs.curriculum_Id = :curriculumId AND u.role='TEACHER'",
             nativeQuery = true)
     List<UserInterface> getTeachersPerSubjectAndCurriculum(@Param("subjectId") Long subjectId, @Param("curriculumId") Long curriculumId);
