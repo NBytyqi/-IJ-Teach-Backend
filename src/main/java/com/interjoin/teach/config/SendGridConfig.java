d663510c23792d214cfb7d9183a4c9776448259c
Nderim.Bytyqi <bytyqinderim87@gmail.com>
2022-09-05 16:45:29 +0200

    added sendgrid

--- src/main/java/com/interjoin/teach/config/SendGridConfig.java
+++ src/main/java/com/interjoin/teach/config/SendGridConfig.java
@@ -1,20 +1,94 @@
 package com.interjoin.teach.config;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.*;
 import com.sendgrid.SendGrid;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Base64;
+
 @Configuration
+@RequiredArgsConstructor
 public class SendGridConfig {
 
-    @Value("${spring.sendgrid.api-key}")
-    String sendGridApiKey;
+    private final AWSCredentialsConfig cognitoCreds;
 
     @Bean
     public SendGrid configSendgrid() {
-        System.out.println("Sendgrid key " + sendGridApiKey);
-        return new SendGrid(sendGridApiKey);
+        String SENDGRID_KEY = getSecret();
+        System.out.println("Sendgrid key: " + SENDGRID_KEY);
+        return new SendGrid(SENDGRID_KEY);
+    }
+
+    public String getSecret() {
+
+        String secretName = "Sendgrid";
+        String region = "eu-west-2";
+        String SENDGRID_KEY = "";
+
+        AWSCredentialsProvider credentials =
+                new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(cognitoCreds.getAwsAccessKey(), cognitoCreds.getAwsSecretKey()));
+
+
+        // Create a Secrets Manager client
+        AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(credentials)
+                .build();
+
+        // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+        // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        // We rethrow the exception by default.
+
+        String secret = null, decodedBinarySecret = null;
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+                .withSecretId(secretName);
+        GetSecretValueResult getSecretValueResult = null;
+
+        try {
+            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+        } catch (DecryptionFailureException e) {
+            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InternalServiceErrorException e) {
+            // An error occurred on the server side.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InvalidParameterException e) {
+            // You provided an invalid value for a parameter.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InvalidRequestException e) {
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (ResourceNotFoundException e) {
+            // We can't find the resource that you asked for.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        }
+
+        // Decrypts secret using the associated KMS key.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if (getSecretValueResult.getSecretString() != null) {
+            secret = getSecretValueResult.getSecretString();
+        }
+        else {
+            decodedBinarySecret = new String(Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
+        }
+
+        JSONObject json = new JSONObject(secret);
+
+        return json.getString("SENDGRID_KEY");
     }
 
 }
