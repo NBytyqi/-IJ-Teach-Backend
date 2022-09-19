8e36e70f74c975e8a27b3f28bc380f03c4d245eb
Nderim.Bytyqi <bytyqinderim87@gmail.com>
2022-09-19 10:56:14 +0200

    changed the otprequest variable name to uuid

--- src/main/java/com/interjoin/teach/services/UserService.java
+++ src/main/java/com/interjoin/teach/services/UserService.java
@@ -624,7 +624,7 @@ public class UserService {
     }
 
     public void verifyUser(OtpVerifyRequest request) throws InterjoinException {
-        User user = repository.findByCognitoUsername(request.getUuid()).orElseThrow(() -> new InterjoinException(String.format("User with uuid: [%s] doesn't exist", request.getCognitoUsername()), HttpStatus.BAD_REQUEST));
+        User user = repository.findByCognitoUsername(request.getUuid()).orElseThrow(() -> new InterjoinException(String.format("User with uuid: [%s] doesn't exist", request.getUuid()), HttpStatus.BAD_REQUEST));
         if(user.isVerifiedEmail()) {
             throw new InterjoinException("User is already verified", HttpStatus.BAD_REQUEST);
         }
